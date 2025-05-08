fb64e799d12e6198f3ca58574396260b18fdc739
Antoni <antfacca@gmail.com>
2025-05-09 01:42:54 +0200

    dupa

--- ising/ANNNI.py
+++ ising/ANNNI.py
@@ -12,7 +12,7 @@ from numpy.polynomial.chebyshev import chebinterpolate
 
 
 
-def create_Tz(L, kappa, beta):
+def create_Tz(L, J, kappa, beta):
     T = lil_matrix((4**L, 4**L), dtype=float)
     for i in range(4**L):
         for j in range(2**L):
@@ -34,7 +34,7 @@ def create_Tx(L, J, beta, kappa_1):
     for i in range(2**L):
         i_bin = np.array([int(bin) for bin in format(i, f'0{L+2}b')]) 
         i_bin = i_bin * 2 - 1
-
+        en_hor = 0  
         for j in range(L):
             en_hor += - kappa_1 * J * i_bin[j] * i_bin[(j + 1) % L]
 
@@ -42,20 +42,41 @@ def create_Tx(L, J, beta, kappa_1):
             a_bin = np.array([int(bin) for bin in format(a, f'0{L+2}b')])
             a_bin = a_bin * 2 - 1
 
-            en_vert = - J * np.sum(a_bin * i_bin)
+            en_vert = - np.sum(a_bin * i_bin)
+            
+            a = (i << L) + a
+            T[a, a] += np.exp(- beta * (en_hor + en_vert))
 
-            T[i, a] += np.exp(- beta * (en_hor + en_vert))
+    return T
 
 
 def update_matrices(L, J, K, kappa, kappa_1, h0, h1, beta):
-    Tz = create_Tz
-    Tx = create_Tx
-    return Tz, Tx
+    Tz = create_Tz(L, J, kappa, beta)
+    Tx = create_Tx(L, J, beta, kappa_1)
+    Tz_1 = create_Tz(L, J, kappa_1, beta)
+    Tx_1 = create_Tx(L, J, beta, kappa)
+    return Tz, Tx, Tx_1, Tz_1
 
 
-def vector_multiplication(v, Tz_1, Tx_1, Tz, Tx):
-    v = Tx_1 @ Tz_1 @ Tx @ Tz
+def vector_multiplication(v, L, Tz_1, Tx_1, Tz, Tx):
+    v = Tx_1 @ Tz_1 @ Tx @ Tz @ v
     return v
+
+L = 4
+J = 1
+K = 1
+kappa = 1
+kappa_1 = 1
+h0 = 0
+h1 = 0
+beta = 1
+
+Tz, Tx, Tx_1, Tz_1 = update_matrices(L, J, K, kappa, kappa_1, h0, h1, beta)
+T = Tx_1 @ Tz_1
+print(T.todense())
+print(np.allclose(Tz_1.todense(), Tz_1.T.todense()))
+
+
 
 
 
