db85139c48470b8c7de3f48f6d8ab35c3770bef9
Antoni <antfacca@gmail.com>
2025-05-21 10:37:11 +0200

    goryca

--- ising/goryca.py
+++ ising/goryca.py
@@ -1,3 +1,17 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from scipy.sparse.linalg import LinearOperator
+
+from scipy.sparse import identity, diags, csr_matrix, lil_matrix
+from scipy.sparse import kron as skron
+from scipy.sparse import linalg as sla
+from scipy import linalg as la
+from numpy.polynomial.chebyshev import Chebyshev
+from numpy.polynomial.chebyshev import chebinterpolate
+
+
+
 def Dhalf_horizontal_TM(L, J, beta, h0, h1, pbc = True, fmt = 'csr'):
 ## site-site horizontal interaction matrix (finite space-direction L)
 ## only diagonal, simplified construction
@@ -31,11 +45,31 @@ def Dhalf_horizontal_TM(L, J, beta, h0, h1, pbc = True, fmt = 'csr'):
 
     return Dhalf
 
-def horizontal_TM(L, J, beta, h0, h1, pbc = True, fmt = 'csr'):
+def create_T(L, kappa, kappa_1, K, beta):
+    #T_z,1 - contribution to the interlayer interaction of first spin
+    T = lil_matrix((2**L, 2**L), dtype=float)
+    for i in range (2**L):
+        a = (i & (~(1 << (L+1)))) + ((2 & i) << L) 
+
+        a_bin = np.array([int(bin) for bin in format(a, f'0{L+2}b')])
+        i_bin = np.array([int(bin) for bin in format(i, f'0{L+2}b')])
+        
+        a_bin = a_bin * 2 - 1
+        i_bin = i_bin * 2 - 1
+        
+        T[i, a] = np.exp(beta * K * a_bin[0] * i_bin[0] - beta * kappa * K * i_bin[0] * a_bin[-2] - beta * kappa_1 * K * i_bin[0] * a_bin[1])
+
+        a = a ^ 2
+        a_bin[-2] *= -1
+        
+        T[i, a] = np.exp(beta * K * a_bin[0] * i_bin[0] - beta * kappa * K * i_bin[0] * a_bin[-2] - beta * kappa_1 * K * i_bin[0] * a_bin[1])
+        
+    return T
 
 
+T = create_T(4, 1, 1, 1, 1)
 
-
+print(np.allclose(T.todense(), T.T.todense()))
 
 
 
