3b612d253c33c4daa655c334721871335e584d6b
Antoni <antfacca@gmail.com>
2025-06-03 00:43:34 +0200

    wielki sukces

--- ising/goryca.py
+++ ising/goryca.py
@@ -10,42 +10,64 @@ from numpy.polynomial.chebyshev import Chebyshev
 from numpy.polynomial.chebyshev import chebinterpolate
 
 
-
-def Dhalf_horizontal_TM(L, J, beta, h0, h1, pbc = True, fmt = 'csr'):
-## site-site horizontal interaction matrix (finite space-direction L)
-## only diagonal, simplified construction
-
+def construct_transfer_matrix(L, K, kappa, kappa_1, beta):
     # Generate all possible spin states for L spins
     states = np.array([list(format(i, f'0{L}b')) for i in range(2 ** L)], dtype=int)
     
-    # Initialize the diagonal of the transfer matrix with zeros
+    # Initialize the transfer matrix with zeros
     size = 2 ** L
-    Diagonal = np.zeros(size, dtype=float)
-
+    T = np.zeros((size, size), dtype=float)
     
     for i in range(size):
-        spins = 2 * states[i] - 1
+        for j in range(size):
+            # Convert spin states to -1 and 1 representation    
+            snew = 2 * states[i] - 1
+            sold = 2 * states[j] - 1 
+            
+            energy = 0.0
+            # Nearest neighbor (horizontal) terms between old and new spins
+            for k in range(L): 
+                if (k % 2 == 1):
+                    energy += (K * snew[k] * snew[(k+1) % L] + kappa * K * snew[k] * snew[(k + 2) % L]) / 2
+                    energy += (K * sold[k] * sold[(k+1) % L] + kappa * K * sold[k] * sold[(k + 2) % L]) / 2
+                if (k % 2 == 0):
+                    energy += (snew[k] * snew[(k+1) % L] + kappa_1 * K * snew[k] * snew[(k + 2) % L]) / 2
+                    energy += (sold[k] * sold[(k+1) % L] + kappa_1 * K * sold[k] * sold[(k + 2) % L]) / 2
 
-        energy = 0.0
+            for k in range(L):
+                if (k % 2 == 0):
+                    energy += K * kappa * snew[k] * sold[k]
+                if (k % 2 == 1):
+                    energy += K * kappa_1 * snew[k] * sold[k]
+                          
+            # Left-up diagonal terms
+            for k in range(1, L, 2): # sites k = 0, 1, ..., L-1
+                energy += K * sold[k] * snew[(k - 1) % L]
+                energy += sold[k] * snew[(k + 1) % L]
 
-        for k in range(L-1):
-            energy += spins[k] * spins[k+1]
-            if k % 2 == 0:
-                energy += h0 * spins[k]
-            else:
-                energy += h1 * spins[k]
+            T[i, j] = np.exp(- beta * energy)
     
-        if ( pbc ):
-            energy += spins[L-1] * spins[0]
+    return T
 
-        Diagonal[i] = energy
-         
-    Dhalf = diags( np.exp( beta * J/2 * Diagonal ), format = fmt  )
 
-    return Dhalf
+def horizontal(L, kappa, kappa_1, K, beta):
+    T = lil_matrix((2**L, 2**L), dtype=float)
+    for a in range(2**L):
+        a_bin = np.array([int(bin) for bin in format(a, f'0{L}b')])
+        a_bin = a_bin * 2 - 1
+        en = 0
+        for j in range(L):
+            if (j % 2 == 1):
+                en += beta * K * a_bin[j] * a_bin[(j + 1) % L] + kappa * beta * K * a_bin[j] * a_bin[(j + 2) % L]
+            if (j % 2 == 0):
+                en += beta * a_bin[j] * a_bin[(j + 1) % L] + kappa_1 * beta * K * a_bin[j] * a_bin[(j + 2) % L]
+        T[a, a] = np.exp(-en / 2)
+
+    return T
+
+
 
 def create_T(L, kappa, kappa_1, K, beta):
-    #T_z,1 - contribution to the interlayer interaction of first spin
     T = lil_matrix((2**L, 2**L), dtype=float)
     for i in range (2**L):
         a = i & (~(1 << (L-1)))
@@ -55,13 +77,11 @@ def create_T(L, kappa, kappa_1, K, beta):
         a_bin = a_bin * 2 - 1
         i_bin = i_bin * 2 - 1
         
-        T[i, a] =  np.exp( beta * kappa * K * i_bin[0] * a_bin[-1] - beta * kappa_1 * K * i_bin[0] * a_bin[1])
-        print(i, a)
+        T[i, a] = np.exp(- kappa * beta * K * i_bin[0] * a_bin[0] - beta * i_bin[0] * a_bin[-1] - beta * K * i_bin[0] * a_bin[1])
         a = a ^ (1 << (L-1))
         a_bin[0] *= -1
-        print(i, a)
 
-        T[i, a] = np.exp( beta * kappa * K * i_bin[0] * a_bin[-1] - beta * kappa_1 * K * i_bin[0] * a_bin[1])
+        T[i, a] = np.exp(- kappa * beta * K * i_bin[0] * a_bin[0] - beta * i_bin[0] * a_bin[-1] - beta * K * i_bin[0] * a_bin[1])
         
     return T
 
@@ -75,12 +95,12 @@ def create_T_single(L, kappa, kappa_1, K, beta):
         a_bin = a_bin * 2 - 1
         i_bin = i_bin * 2 - 1
         
-        T[i, a] =  np.exp(beta * K * a_bin[0] * i_bin[0])
+        T[i, a] =  np.exp(- kappa_1 * beta * K * a_bin[0] * i_bin[0])
 
         a = a ^ (1 << (L-1))
         a_bin[0] *= -1
         
-        T[i, a] = np.exp(beta * K * a_bin[0] * i_bin[0])
+        T[i, a] = np.exp(- kappa_1 * beta * K * a_bin[0] * i_bin[0])
 
     return T
 
@@ -109,23 +129,29 @@ def reverse(L):
     
 
 fmt = 'csr'
-L = 4
-T = create_T(L, 1, 1, 1, 1)
-T_single = create_T_single(L, 1, 1, 1, 1)
+L = 6
+kappa = 1
+kappa_1 = 1
+K = 1
+beta = 1
+T = create_T(L, kappa, kappa_1, K, beta)
+T_single = create_T_single(L, kappa, kappa_1, K, beta)
+Dh_2 = horizontal(L, kappa, kappa_1, K, beta)
 P = cyclic_shift(L)
 P1 = reverse_cyclic_shift(L) 
 R = reverse(L)
-print(R.todense())
 
 Trans_2_site = T_single @ P1 @ T
 Trans = identity(2**L, format = fmt) 
+Trans = P @ P @ T @ Trans
 
-for i in range(int(L / 2)):
-    Ttmp = P @ P @ T @ Trans
+for i in range(int(L / 2 - 1)):
+    Ttmp = P @ P @ P @ Trans_2_site @ Trans
     Trans = Ttmp
 
-Trans = P @ P @ R @ Trans
+Trans = Dh_2 @ P @ T_single @ P1 @ Trans @ Dh_2
 
-print(Trans.todense())
-print(np.allclose(Trans.todense(), Trans.T.todense()))
+T_reference = construct_transfer_matrix(L, K, kappa, kappa_1, beta)
+
+print(np.allclose(Trans.todense(), T_reference))
 
